ae5d3ec0e7f0a1da90deac1c5978a8bd17810a42
unknown-cstdio <jp95@rice.edu>
2024-02-05 00:52:09 -0600

    a

--- broker/ipc.go
+++ broker/ipc.go
@@ -234,7 +234,7 @@ func (i *IPC) ClientOffers(arg messages.Arg, response *[]byte) error {
 		ip, _, _ := net.SplitHostPort(url)
 
 		//testing
-		ip = "13.59.55.180"
+		//ip = "13.59.55.180"
 
 		log.Printf("Client: Matched with %s", ip)
 		offerJSON, err := json.Marshal(offer)
@@ -288,8 +288,8 @@ func (i *IPC) ClientOffers(arg messages.Arg, response *[]byte) error {
 						client.proxy = oldProxy
 						continue
 					}
-					//transferReq := messages.TransferRequest{Cid: client.id, NewIp: client.proxy.ip, TransferNow: true}
-					transferReq := messages.TransferRequest{Cid: client.id, NewIp: "18.118.29.29:51821", TransferNow: true}
+					transferReq := messages.TransferRequest{Cid: client.id, NewIp: client.proxy.ip, TransferNow: true}
+					//transferReq := messages.TransferRequest{Cid: client.id, NewIp: "18.118.29.29:51821", TransferNow: true}
 					transferReqJSON, err := json.Marshal(transferReq)
 					if err != nil {
 						log.Printf("error marshalling transfer request")
@@ -297,7 +297,7 @@ func (i *IPC) ClientOffers(arg messages.Arg, response *[]byte) error {
 					}
 					oldIp, _, _ := net.SplitHostPort(oldProxy.ip)
 					//testing
-					oldIp = "13.59.55.180"
+					//oldIp = "13.59.55.180"
 					transferPath := fmt.Sprintf("http://%s:%s/transfer", oldIp, newPort)
 					log.Printf("sending transfer request to %s", transferPath)
 					resp, err := http.Post(transferPath, "application/json", bytes.NewBuffer(transferReqJSON))
